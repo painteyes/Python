c2f7e696c99b8b939845681a69f08f728d84e1ff
Andrea Occhipinti <andreantoniocchipinti@gmail.com>
2023-03-20 12:55:40 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        numero1 = int(input("Inserisci il primo numero: "))
+        break
+    except ValueError:
+        print("Devi inserire un numero intero, riprova.")
+
+while True:
+    numero2 = input("Inserisci il secondo numero: ")
+    if numero2.isdigit():
+        numero2 = int(numero2)
+        break
+    else:
+        print("Devi inserire un numero intero, riprova.")
+    
+print("Il primo numero inserito è:", numero1)
+print("Il secondo numero inserito è:", numero2)
